e5a2f0e4c413f5ecd6f30fbc7c91d1f6b75b9890
Suryansh Saini <suryansh19saini@gmail.com>
2026-01-11 13:59:26 +0530

    Solved leet code problem

---
+++ 50_stacks_lc4.c
@@ -0,0 +1,50 @@
+// ---------------Leet Code problem - Insert an element at bottom of the stack.--------------------
+
+#include<stdio.h>
+
+void push(int stack[], int *stkptr, int num){
+    if(*stkptr >= 10){
+        printf("Stack Overflowed\n");
+        return;
+    }
+    stack[(*stkptr)++] = num;
+}
+
+int pop(int stack[], int *stkptr){
+    (*stkptr)--;
+    return stack[*stkptr];
+}
+
+void print(int stack[], int stkptr){
+    printf("[");
+    while(stkptr--){
+        printf("%d", stack[stkptr]);
+        if(stkptr > 0){
+            printf(", ");
+        }
+    }
+    printf("]\n");
+}
+
+void insert(int stack[], int *stkptr, int num){
+    if(*stkptr == 0){
+        stack[(*stkptr)++] = num;
+        return;
+    }
+    int temp = pop(stack, stkptr);
+    insert(stack, stkptr, num);
+    push(stack, stkptr, temp);
+}
+
+int main(){
+    int stack[10];
+    int stkptr = 0;
+    push(stack, &stkptr, 2);            
+    push(stack, &stkptr, 3);            
+    push(stack, &stkptr, 4);            
+    push(stack, &stkptr, 5);
+    print(stack, stkptr);
+    insert(stack, &stkptr, 1); 
+    print(stack, stkptr);
+    return 0;
+}
